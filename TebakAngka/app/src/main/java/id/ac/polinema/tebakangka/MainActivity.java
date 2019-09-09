6cfd9fbc69d7749a0a26fd76d67f770dd4e65f5d
arikyahya <arikyahya02@gmail.com>
2019-09-09 10:10:08 +0700

    tebak angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -12,25 +12,40 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-	//  TODO: deklarasikan variabel di sini
+    private EditText number;
+    private Button guess, reset;
+    int random = 0;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		// TODO: bind layout di sini
-	}
 
-	// TODO: generate angka random di sini
-	private void initRandomNumber() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initRandomNumber();
+        number = findViewById(R.id.number_input);
+        guess = findViewById(R.id.guess_button);
+        reset = findViewById(R.id.reset_button);
+    }
 
-	}
+    // TODO: generate angka random di sini
+    private int initRandomNumber() {
+        random = (int) (Math.random() * 100);
+        return random;
+    }
 
-	public void handleGuess(View view) {
-		// TODO: Tambahkan logika untuk melakukan pengecekan angka
-	}
+    public void handleGuess(View view) {
+        int numberr = Integer.parseInt(String.valueOf(number.getText()));
+        if (numberr > random) {
+            Toast.makeText(this, "Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
+        } else if (numberr < random) {
+            Toast.makeText(this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Tebakan anda benar!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-	public void handleReset(View view) {
-		// TODO: Reset tampilan
-	}
+    public void handleReset(View view) {
+        number.setText("");
+        initRandomNumber();
+    }
 }
